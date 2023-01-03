87788ad97623ff436ba3550544c5924c16360be0
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2023-01-03 20:13:44 +0100

    Optimized images

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://www.valtech.com/fr-fr/"
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg"
+              backgroundImageUrl="/images/pexels-photo-189349-min.jpeg"
               visibleParagraph={true}
               hover={true}
             />
@@ -38,7 +38,7 @@ export default function Home() {
               paragraph="Venice, the capital of northern Italy's Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/3546189/pexels-photo-3546189.jpeg"
+              backgroundImageUrl="/images/pexels-nextvoyage-3546189-min.jpg"
               visibleParagraph={false}
               hover={true}
             />
@@ -50,7 +50,7 @@ export default function Home() {
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/2570063/pexels-photo-2570063.jpeg"
+              backgroundImageUrl="/images/pexels-photo-2570063-min.jpeg"
               visibleParagraph={false}
               hover={true}
             />
@@ -62,7 +62,7 @@ export default function Home() {
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/8204190/pexels-photo-8204190.jpeg"
+              backgroundImageUrl="/images/pexels-photo-8204190-min.jpeg"
               visibleParagraph={false}
               hover={true}
             />
@@ -74,19 +74,19 @@ export default function Home() {
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg"
+              backgroundImageUrl="/images/pexels-photo-460740-min.jpeg"
               visibleParagraph={false}
               hover={true}
             />
           </div>
           <div className={styles.div6}>
             <Card
-              subtitle="ITALY"
-              title="VENICE"
+              subtitle="NETHERLAND"
+              title="AMSTERDAM"
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/3546189/pexels-photo-3546189.jpeg"
+              backgroundImageUrl="/images/pexels-photo-1790573-min.jpeg"
               visibleParagraph={false}
               hover={true}
             />
@@ -98,7 +98,7 @@ export default function Home() {
               paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
               buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
               buttonText={DEFAULT_BUTTON_TEXT}
-              backgroundImageUrl="https://images.pexels.com/photos/10960259/pexels-photo-10960259.jpeg"
+              backgroundImageUrl="/images/pexels-photo-10960259-min.jpeg"
               visibleParagraph={false}
               hover={true}
             />
