795254e14567329f6544560de190a8c2bae9118e
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2023-01-03 19:15:11 +0100

    Fix Card test

--- components/Card.cy.tsx
+++ components/Card.cy.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Card from "./Card";
 
-const DEFAULT_BUTTON_TEXT = "Explore More"
+const DEFAULT_BUTTON_TEXT = "Explore More";
 
 describe("<Card />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(
       <Card
-        subtitle="ITALY"
-        title="VENICE"
-        paragraph="Venice, the capital of northern Italy's Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals."
-        buttonUrl="https://fr.wikipedia.org/wiki/Venise#:~:text=Venise%20(en%20italien%20%3A%20Venezia%20%2F,et%20reli%C3%A9es%20par%20435%20ponts."
+        subtitle="FRONT-END"
+        title="valtech_"
+        paragraph="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        buttonUrl="https://www.valtech.com/fr-fr/"
         buttonText={DEFAULT_BUTTON_TEXT}
-        backgroundImageUrl="https://images.pexels.com/photos/3546189/pexels-photo-3546189.jpeg"
+        backgroundImageUrl="https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg"
+        visibleParagraph={true}
+        hover={true}
       />
     );
   });
